62e794a1638fd6a25a36f3b4b80ec4c2cd10ee93
ben stueck <8847113+willbestuck@users.noreply.github.com>
2022-03-27 09:52:09 -0700

    using generated access token from spotify developer site, created generated playlsit

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const csv = require('./functions/csv_intake.js');
+const spotify = require('./functions/spotify.js');
+const playlistId = '6fhqad9fZXrhgUxE1sDO9J';
 
 async function main() {
     const artists = await csv();
+    const token = await spotify.getToken();
 
-    for (obj of artists) {
-        
+    const ids = [];
+
+    for (obj of artists.slice(140,160)) {
+        let reqId = await spotify.getArtist(token, obj);
+        if (reqId !== null) {ids.push(reqId);}
     }
+
+    const tracks = await spotify.getTracks(token, ids);
+    
+    const status = await spotify.addTracksToPlaylist(token, tracks, playlistId);
+
+    console.log(status);
 }
 
 main().catch((err) => {
--- functions/spotify.js
+++ functions/spotify.js
@@ -1,32 +1,81 @@
 require('dotenv').config()
-const request = require('request');
+const axios = require('axios');
 const clientId = process.env.SPOTIFY_CLIENT_ID;
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
-const authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
+exports.getToken = async () => {
+    try {
+        const tokenRes = await axios.post('https://accounts.spotify.com/api/token', 'grant_type=client_credentials', {
+            headers: {
+                'Authorization': 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64')),
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+    
+        return tokenRes.data.access_token;
+        
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
+    } catch(err) {
+        console.log(err.response);
+        return '';
+    }
+}
 
-    const token = body.access_token;
-    const options = {
-      url: 'https://api.spotify.com/v1/search?q=artist:deadmau5&type=artist&include_external=audio',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      console.log(body.artists.items);
-    });
-  }
-});
+exports.getArtist = async (token, artistName) => {
+
+    try {
+
+        const artistRes = await axios.get('https://api.spotify.com/v1/search?q=artist:'+artistName+'&type=artist&include_external=audio', {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+
+        return artistRes.data.artists.items[0].id;
+        
+
+    } catch(err) {
+        console.log(artistName);
+        return null;
+    }
+
+}
+
+exports.getTracks = async (token, artistIds) => {
+    const tracks = [];
+    for (id of artistIds) {
+        try {
+            const trackRes = await axios.get('https://api.spotify.com/v1/artists/'+id+'/top-tracks?market=US', {
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                }
+            })
+            
+            for (t of trackRes.data.tracks.slice(0,5)) {
+                tracks.push(t.uri);
+            }
+    
+        } catch(err) {
+            console.log('err', id);
+        }
+    }
+    return tracks;
+}
+
+exports.addTracksToPlaylist = async (token, tracks, playlistId) => {
+    try {
+        const playlistRes = await axios.post('https://api.spotify.com/v1/playlists/'+playlistId+'/tracks', tracks, {
+            headers: {
+                'Authorization': 'Bearer BQA3yatXL4k5U-vLL3JvOMbrHVEaNKX9VafycapAeErb2KuOz_JGE8RY48wyg0Pr4Cu9BV6n1Ms6PIf-BHkI0SiJNNWwtUxad_XKhUDOftkkTX2OofjDJB8pDc5qbuPXNlEqlzVWUFwq8WqlWy6cBTmxf6UrHEFDTSugCWTQhvMdXlIE_szY9-nhWj8PMyphyDYfGB7hRZqXv5rPLjgw6JM',
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        return 'Success';
+        
+
+    } catch(err) {
+        console.log(err.response);
+        return 'Failure';
+    }
+}
